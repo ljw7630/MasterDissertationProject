5413395cecaf8d303c81a70a68521cae4bc771bb
Jinwu <ljw7630@hotmail.com>
2013-07-25 12:53:29 +0100

    url pattern

--- website/website/urls.py
+++ website/website/urls.py
@@ -8,12 +8,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       ('^survey/hello/$', views.hello),
-                       ('^survey/$', views.hello),
-                       ('^survey/consent/$', views.consent),
-                       ('^survey/print_users/$', 'survey.views.print_users'),
-                       ('^survey/print_form/$', 'survey.views.print_form'),
-                       ('^survey/compare/$', 'survey.views.compare'),
+                       ('^hello/$', views.hello),
+                       ('^$', views.hello),
+                       ('^consent/$', views.consent),
+                       ('^print_users/$', 'survey.views.print_users'),
+                       ('^print_form/$', 'survey.views.print_form'),
+                       ('^compare/$', 'survey.views.compare'),
                        # Examples:
                        # url(r'^$', 'website.views.home', name='home'),
                        # url(r'^website/', include('website.foo.urls')),
