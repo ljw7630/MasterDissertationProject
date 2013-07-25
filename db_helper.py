2d11c1fa09d3847a1ece45237c6c65293759274f
Jinwu <ljw7630@hotmail.com>
2013-07-25 11:59:47 +0100

    update DBHelper

--- db_helper.py
+++ db_helper.py
@@ -5,9 +5,22 @@ class DBHelper():
 
 	@staticmethod
 	def getCursor():
-		DBHelper.con = sqlite3.connect('website/website/db/website.db')
-		DBHelper.cur = DBHelper.con.cursor()
-		return DBHelper.cur
+		try:
+			return DBHelper.cur
+		except AttributeError:
+			DBHelper.con = sqlite3.connect('website/website/db/website.db')
+			DBHelper.cur = DBHelper.con.cursor()
+			return DBHelper.cur
+
+	@staticmethod
+	def getNotExistFileNames(limit=1000):
+		res = DBHelper.getCursor().execute("select file_name from survey_data where file_exists=0 limit=%i" % limit)
+		return res.fetchall()
+
+	@staticmethod
+	def getNotRDFedFileName(limit=1000):
+		res = DBHelper.getCursor().execute("select file_name from survey_data where file_exists=1 and rdf=0 limit=%i" % limit)
+		return res.fetchall()
 
 	@staticmethod
 	def dataAddEntry(file_name, url, exist, type='PERSON'):
@@ -18,18 +31,18 @@ class DBHelper():
 			DBHelper.con.commit()
 		except sqlite3.IntegrityError:
 			if exist:
-				DBHelper.getCursor().execute("update survey_data set file_exists = %i" % int(exist))
+				DBHelper.getCursor().execute("update survey_data set file_exists=%i" % int(exist))
 				DBHelper.con.commit()
 			else:
 				raise sqlite3.IntegrityError
 
 	@staticmethod
 	def dataSetRDF(file_name):
-		DBHelper.getCursor().execute("update survey_data set rdf='True' where file_name=%s" % file_name)
+		DBHelper.getCursor().execute("update survey_data set rdf=1 where file_name=%s" % file_name)
 
 	@staticmethod
 	def isDataRDFed(file_name):
-		res = DBHelper.getCursor().execute("select count(*) from survey_data where file_name = '%s' and rdf='False'"
+		res = DBHelper.getCursor().execute("select count(*) from survey_data where file_name='%s' and rdf=0"
 			% file_name)
 		count = res.fetchone()[0]
 		if count != 0:
@@ -52,5 +65,8 @@ class DBHelper():
 			DBHelper.cur.close()
 			DBHelper.con.commit()
 			DBHelper.con.close()
+
+			del DBHelper.cur
+			del DBHelper.con
 		except AttributeError:
 			pass
