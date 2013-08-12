149228ae282e2f3aed5957e63418d97dc1930f7f
Jinwu <ljw7630@hotmail.com>
2013-08-12 12:14:13 +0100

    change setting

--- website/website/settings.py
+++ website/website/settings.py
@@ -36,7 +36,7 @@ MANAGERS = ADMINS
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': APP_PATH + "/db/website.db",					  # Or path to database file if using sqlite3.
+		'NAME': APP_PATH + "/db/survey.db",					  # Or path to database file if using sqlite3.
 		# The following settings are not used with sqlite3:
 		'USER': '',
 		'PASSWORD': '',
